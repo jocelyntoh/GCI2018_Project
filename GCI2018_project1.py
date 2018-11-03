7ec752b77f197d9c9520c91f9639777e16322de4
jocelyntoh <44668284+jocelyntoh@users.noreply.github.com>
2018-11-03 15:31:15 +0800

    Update GCI2018_project1.py

--- GCI2018_project1.py
+++ GCI2018_project1.py
@@ -1,6 +1,6 @@
 for counter in range(10):
         print ("GCI is great")
-        userName=input("What is your name? ")
-        print("Hello " + userName + " , please to meet you!")
-        reverseName=userName[::-1]
-        print("Did you know that your name backwards is {}?".format(reverseName))
+userName=input("What is your name? ")
+print("Hello " + userName + " , please to meet you!")
+reverseName=userName[::-1]
+print("Did you know that your name backwards is {}?".format(reverseName))
